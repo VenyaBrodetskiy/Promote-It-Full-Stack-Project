066341eddb09a11538ca1130a894e8da6b213dea
milaShurupova <mila.shurupova@gmail.com>
2022-12-26 23:19:14 +0200

    campaign.controller created, discuss validations

---
+++ Backend/NodeJs-server/source/modules/campaign/campaign.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { NON_EXISTING_ID } from "../../common/constants";
+import { campaign, AuthenticatedRequest, systemError } from "../../common/entities";
+import { ResponseHelper } from "../../core/helpers/response.helper";
+import CampaignService from "./campaign.service";
+
+class CampaignController {
+
+
+    constructor() { }
+
+    public addCampaign(req: Request, res: Response, next: NextFunction) {
+
+        // ?? add validation, that userId is type of non_profit organization
+        // ?? add validation, that landing page is working one 
+
+        
+        const body: campaign = req.body;
+        const inputCampaign = {
+            id: NON_EXISTING_ID,
+            hashtag: body.hashtag,
+            landing_page: body.landing_page
+        };
+
+        CampaignService.addCampaign(inputCampaign, (req as AuthenticatedRequest).userData.userId)
+            .then((result: campaign) => {
+                return res.status(200).json(result);
+            })
+            .catch((error: systemError) => {
+                return ResponseHelper.handleError(res, error);
+            })
+    }
+
+}
+
+
+export default new CampaignController();
