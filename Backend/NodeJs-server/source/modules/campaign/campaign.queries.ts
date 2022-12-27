905d944e80432da587620aef079e23f458563107
milaShurupova <mila.shurupova@gmail.com>
2022-12-27 16:58:03 +0200

    added query GetAllCampaignsWitnProducts

--- Backend/NodeJs-server/source/modules/campaign/campaign.queries.ts
+++ Backend/NodeJs-server/source/modules/campaign/campaign.queries.ts
@@ -5,6 +5,22 @@ export class CampaignQueries {
         FROM [dbo].[campaign]
         INNER JOIN [non_profit_organization] ON [campaign].[user_id] = [non_profit_organization].[user_id]
         WHERE [campaign].[status_id] = ?`;
+    
+    public static GetAllCampaignsWitnProducts: string = `
+        SELECT 
+            [campaign].[id], 
+            [hashtag], 
+            [landing_page], 
+            [non_profit_organization].[name] as non_profit_organization_name, 
+            [title] as product_title, 
+            [business_owner].[name] as business_owner_name, 
+            [product_qty]
+        FROM [dbo].[campaign]
+        INNER JOIN [non_profit_organization] ON [campaign].[user_id] = [non_profit_organization].[user_id]
+        LEFT JOIN [product_to_campaign_qty] ON [campaign].[id] = [product_to_campaign_qty].[campaign_id]
+        LEFT JOIN [product] ON [product_to_campaign_qty].[product_id] = [product].[id]
+        LEFT JOIN [business_owner] ON [product].[user_id] = [business_owner].[user_id]
+        WHERE [campaign].[status_id] = ?`;
 
     public static AddCampaign: string = `
         INSERT campaign 
