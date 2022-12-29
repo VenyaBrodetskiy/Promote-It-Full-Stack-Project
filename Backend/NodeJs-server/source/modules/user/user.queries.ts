f8e6b922b2887e2b18519eb60c88fc17b046639f
milaShurupova <mila.shurupova@gmail.com>
2022-12-30 00:03:25 +0200

    created queries for user adding all 4 pcs

--- Backend/NodeJs-server/source/modules/user/user.queries.ts
+++ Backend/NodeJs-server/source/modules/user/user.queries.ts
@@ -1,5 +1,40 @@
 export class UserQueries {
-    // Queries for User management
+
+    public static AddUser: string = `
+        INSERT [user] 
+            (login, password, user_type_id,
+            create_date, update_date, 
+            create_user_id, update_user_id, 
+            status_id) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    
+    public static AddSocialActivist: string = `
+        INSERT [social_activist] 
+            (user_id, twitter_handle, email,
+            address, phone_number,
+            create_date, update_date, 
+            create_user_id, update_user_id, 
+            status_id) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    
+    public static AddBusinessOwner: string = `
+        INSERT [business_owner] 
+            (user_id, twitter_handle, name, email,
+            create_date, update_date, 
+            create_user_id, update_user_id, 
+            status_id) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    
+    public static AddNonProfitOrganization: string = `
+        INSERT [non_profit_organization] 
+            (user_id, name, email, website,
+            create_date, update_date, 
+            create_user_id, update_user_id, 
+            status_id) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    
+    // delete below ->
+    
     public static GetAll: string = `
         SELECT u.id, first_name, last_name, [login], r.role_name
         FROM [user] AS u
@@ -11,14 +46,7 @@ export class UserQueries {
         UPDATE [user] 
         SET first_name = ?, last_name = ?, update_date = ?, update_user_id = ? 
         WHERE id = ? AND status_id = ?`;
-    public static AddUser: string = `
-        INSERT [user] 
-            (first_name, last_name, 
-            login, password,  
-            create_date, update_date, 
-            create_user_id, update_user_id, 
-            status_id) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    
     public static AddRolesToUser: string = `
         INSERT user_to_role
         (user_id, role_id, 
@@ -35,4 +63,5 @@ export class UserQueries {
         UPDATE user_to_role
         SET update_date = ?, update_user_id = ?, status_id = ?
         WHERE user_id = ? AND status_id = ?`;
+
 }
