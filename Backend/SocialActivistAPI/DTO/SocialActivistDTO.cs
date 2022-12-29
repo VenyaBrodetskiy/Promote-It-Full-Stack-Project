b358874d76e5d54afd874c0e94b6d76b942c96f2
Venya Brodetskiy <benjamincheg@gmail.com>
2022-12-29 15:41:17 +0200

    Added DTO, exception handling

---
+++ Backend/SocialActivistAPI/DTO/SocialActivistDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace SocialActivistAPI.DTO
+{
+
+public record SocialActivistDTO
+{
+    public int UserId { get; set; }
+
+    public string TwitterHandle { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+}
+
+}
