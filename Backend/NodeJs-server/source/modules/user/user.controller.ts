80941cbe84cac5d964ea893e5a9b9402c5fbf44f
milaShurupova <mila.shurupova@gmail.com>
2023-01-02 18:53:39 +0200

    refactored, created addSocialActivist

--- Backend/NodeJs-server/source/modules/user/user.controller.ts
+++ Backend/NodeJs-server/source/modules/user/user.controller.ts
@@ -16,32 +16,15 @@ class UserController {
     public addBusinessOwner(req: Request, res: Response, next: NextFunction) {
 
         loggerService.info(`${req.method} ${req.originalUrl}`);
-        const body: userAnyType = req.body;
-        const hashedPasword: string = bcrypt.hashSync(body.password as string);
 
-        const user: user = {
-            id: NON_EXISTING_ID,
-            userTypeId: body.userTypeId,
-            login: body.login,
-            password: hashedPasword,
-        };
-
-        const userInfo: userInfo = {
-            user_id: NON_EXISTING_ID,
-            twitter_handle: body.twitter_handle,
-            name: body.name,
-            email: body.email,
-            // address: body.address,
-            // phone_number: body.phone_number,
-            // website: body.website,
-        };
-
-        
+        const user: user = UserController.parseBodyToUser(req);
+        const userInfo: userInfo = UserController.parseBodyToUserInfo(req);
+    
         UserService.addUser(user, SYSTEM_ID) 
             .then((id: number) => {
                 userInfo.user_id = id;
 
-                return UserService.addUserInfo(userInfo, userInfo.user_id);
+                return UserService.addBusinessOwner(userInfo);
             })
             .then((userInfo: userInfo) => {
                 return res.status(200).json(userInfo.user_id);
@@ -50,7 +33,55 @@ class UserController {
                 return ResponseHelper.handleError(res, error);
             }) 
     }
+
+    public addSocialActivist(req: Request, res: Response, next: NextFunction) {
+
+        loggerService.info(`${req.method} ${req.originalUrl}`);
+
+        const user: user = UserController.parseBodyToUser(req);
+        const userInfo: userInfo = UserController.parseBodyToUserInfo(req);
+
+        UserService.addUser(user, SYSTEM_ID)
+            .then((id: number) => {
+                userInfo.user_id = id;
+
+                return UserService.addSocialActivist(userInfo);
+            })
+            .then((userInfo: userInfo) => {
+                return res.status(200).json(userInfo.user_id);
+            })
+            .catch((error: systemError) => {
+                return ResponseHelper.handleError(res, error);
+            })
+    }
     
+    private static parseBodyToUser(req: Request) {
+
+        const body: userAnyType = req.body;
+        const hashedPasword: string = bcrypt.hashSync(body.password as string);
+        const user: user = {
+            id: NON_EXISTING_ID,
+            userTypeId: body.userTypeId,
+            login: body.login,
+            password: hashedPasword,
+        };
+        return user;
+    }
+
+    private static parseBodyToUserInfo(req: Request) {
+
+        const body: userAnyType = req.body;
+        const userInfo: userInfo = {
+            user_id: NON_EXISTING_ID,
+            twitter_handle: body.twitter_handle,
+            name: body.name,
+            email: body.email,
+            address: body.address,
+            phone_number: body.phone_number,
+            website: body.website,
+        };
+        return userInfo;
+    }
     
 }
 
--- Backend/NodeJs-server/source/modules/user/user.service.ts
+++ Backend/NodeJs-server/source/modules/user/user.service.ts
@@ -70,41 +70,48 @@ class UserService implements IUserService {
         });
     }
 
-    // public addUser(user: user, userId: number): Promise<user> {
-    //     return new Promise<user>((resolve, reject) => {
-    //         const createDate: string = DateHelper.dateToString(new Date());
-            
-    //         SqlHelper.createNew(
-    //             UserQueries.AddUser, user, 
-    //             user.login as string, user.password as string, user.userTypeId,  
-    //             createDate, createDate, 
-    //             userId, userId, 
-    //             Statuses.Active)
-    //         .then((result: entityWithId) => {
-    //             resolve(result as user);
-    //         })
-    //         .catch((error: systemError) => reject(error));
-    //     });
-    // }
+    public addBusinessOwner(userInfo: userInfo): Promise<userInfo> {
+        return new Promise<userInfo>((resolve, reject) => {
+            const createDate: string = DateHelper.dateToString(new Date());
+
+            SqlHelper.executeQueryNoResult(
+                UserQueries.AddBusinessOwner, false,
+                userInfo.user_id, userInfo.twitter_handle!,
+                userInfo.name!, userInfo.email,
+                createDate, createDate,
+                userInfo.user_id, userInfo.user_id,
+                Statuses.Active)
+                .then(() => {
+                    resolve(userInfo);
+                })
+                .catch((error: systemError) => reject(error));
+        });
+    }
+
+    public addSocialActivist(userInfo: userInfo): Promise<userInfo> {
+        return new Promise<userInfo>((resolve, reject) => {
+            const createDate: string = DateHelper.dateToString(new Date());
+
+            SqlHelper.executeQueryNoResult(
+                UserQueries.AddSocialActivist, false,
+                userInfo.user_id, userInfo.twitter_handle!, userInfo.email,
+                userInfo.address!, userInfo.phone_number!,
+                createDate, createDate,
+                userInfo.user_id, userInfo.user_id,
+                Statuses.Active)
+                .then(() => {
+                    resolve(userInfo);
+                })
+                .catch((error: systemError) => reject(error));
+        });
+    }
 
     public addUserInfo(userInfo: userInfo, userTypeId: UserType): Promise<userInfo> {
         return new Promise<userInfo>((resolve, reject) => {
             const createDate: string = DateHelper.dateToString(new Date());
 
             switch (userTypeId) {
-                case UserType.socialActivist: 
-                    SqlHelper.executeQueryNoResult(
-                        UserQueries.AddSocialActivist, false,
-                        userInfo.user_id, userInfo.twitter_handle!, userInfo.email,
-                        userInfo.address!, userInfo.phone_number!,
-                        createDate, createDate,
-                        userInfo.user_id, userInfo.user_id,
-                        Statuses.Active)
-                        .then(() => {
-                            resolve(userInfo);
-                        })
-                        .catch((error: systemError) => reject(error));
-                    break;
+
                 case UserType.businessOwner: 
                     SqlHelper.executeQueryNoResult(
                         UserQueries.AddBusinessOwner, false,
@@ -165,98 +172,15 @@ class UserService implements IUserService {
     //     });
     // }
 
-    // public updateById(user: user, userId: number): Promise<user> {
-    //     return new Promise<user>((resolve, reject) => {
-    //         const updateDate: string = DateHelper.dateToString(new Date());
 
-    //         const [AddRolesExtended, roleParams] = this.prepareQueryToAddRoles(user, updateDate, userId);
-    //         Promise.all([
-    //             // delete old roles of user (table: user_to_role)
-    //             SqlHelper.executeQueryNoResult(
-    //                 UserQueries.DeleteRolesOfUser, true,
-    //                 updateDate, userId,
-    //                 Statuses.NotActive,
-    //                 user.id, Statuses.Active),
-    //             // updates other user property except roles (table: user)
-    //             SqlHelper.executeQueryNoResult(
-    //                 UserQueries.UpdateUserById, false, 
-    //                 user.firstName, user.lastName, 
-    //                 updateDate, userId, 
-    //                 user.id, Statuses.Active)
-    //         ])
-    //         .then(() => {
-    //             // adds new roles to user (table: user_to_role)
-    //             return SqlHelper.executeQueryNoResult(
-    //                 AddRolesExtended as string, false, 
-    //                 ...roleParams, 
-    //                 );
-    //         })
-    //         .then(() => {
-    //             resolve(user);
-    //         })
-    //         .catch((error: systemError) => reject(error));
-    //     });
-    // }
 
     
 
-    // public deleteById(id: number, userId: number): Promise<void> {
-    //     return new Promise<void>((resolve, reject) => {
-    //         const updateDate: string = DateHelper.dateToString(new Date());
 
-    //         // TODO: revise this const temp user to passed from request (by auth)
-    //         Promise.all([
-    //             SqlHelper.executeQueryNoResult(
-    //                 UserQueries.DeleteUserById, true, 
-    //                 updateDate, userId, 
-    //                 Statuses.NotActive, 
-    //                 id, Statuses.Active),
-    //             SqlHelper.executeQueryNoResult(
-    //                 UserQueries.DeleteRolesOfUser, true,
-    //                 updateDate, userId,
-    //                 Statuses.NotActive,
-    //                 id, Statuses.Active
-    //             )
-    //         ])
-    //         .then(() => {
-    //             resolve();
-    //         })
-    //         .catch((error: systemError) => reject(error));
 
-            
-    //     });
-    // }
 
-    // private parseUser(localUser: localUser): user {
-    //     if (typeof localUser.role_name === 'string') localUser.role_name = [localUser.role_name];
-    //     return {
-    //         id: localUser.id,
-    //         firstName: localUser.first_name,
-    //         lastName: localUser.last_name,
-    //         login: localUser.login,
-    //         roles: localUser.role_name
-    //     }
-    // }
 
-    // private prepareQueryToAddRoles(user: user, createDate: string, userId: number): (string | (string | number)[])[] {
-        
-    //     const roles: string[] = user.roles;
-    //     const params: (string | number)[] = [
-    //         user.id, Role[(roles[0] as RoleType)], 
-    //         createDate, createDate,
-    //         userId, userId, Statuses.Active];
-    //     let AddRolesExtended: string = UserQueries.AddRolesToUser;
 
-    //     for (let index = 1; index < roles.length; index++) {
-    //         params.push(
-    //             user.id, Role[(roles[index] as RoleType)], 
-    //             createDate, createDate,
-    //             userId, userId, Statuses.Active);
-    //         AddRolesExtended += ', (?, ?, ?, ?, ?, ?, ?)'
-    //     }
-
-    //     return [AddRolesExtended, params];
-    // }
 }
 
 export default new UserService();
