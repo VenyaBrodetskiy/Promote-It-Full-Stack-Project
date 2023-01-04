2c72fe3f4c070d93f367ebc976b2493862d4d93d
milaShurupova <mila.shurupova@gmail.com>
2023-01-04 20:56:06 +0200

    refactor of GetOrders: created OrderDTO

---
+++ Backend/dotNetBackend/DTO/OrderDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace dotNetBackend.DTO
+{
+    public record OrderDTO
+    {
+        public int UserId { get; set; }
+
+        public string TwitterHandle { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public int ProductId { get; set; }
+
+        public string ProductTitle { get; set; } = null!;
+
+        public string TransactionState { get; set; }
+
+    }
+}
