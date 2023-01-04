7e051ab45e69b2850f9579185f8b28f4cdcdea14
milaShurupova <mila.shurupova@gmail.com>
2023-01-05 00:00:54 +0200

    CreateProduct done without validation

---
+++ Backend/dotNetBackend/DTO/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace dotNetBackend.DTO
+{
+    public record ProductDTO
+    {
+
+        public string Title { get; set; } = null!;
+
+        public int Price { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
