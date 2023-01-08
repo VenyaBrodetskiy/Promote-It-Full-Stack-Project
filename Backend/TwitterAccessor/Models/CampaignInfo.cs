f0cd31703a8f290a05b72158c3c3a36c989a1c44
Venya Brodetskiy <benjamincheg@gmail.com>
2023-01-08 16:24:32 +0200

    added TwitterAccessor project

---
+++ Backend/TwitterAccessor/Models/CampaignInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitterAccessor
+{
+    public record CampaignInfo
+    {
+        public int id { get; set; }
+        public string hashtag { get; set; }
+        public string landingPage { get; set; }
+
+    }
+}
