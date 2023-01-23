aff8879ae94ba9334780aa93bc1fa0c6dcfd1f94
Venya Brodetskiy <benjamincheg@gmail.com>
2023-01-23 13:13:21 +0200

    configuration services moved to separate file

--- Backend/dotNetBackend/MainService/Program.cs
+++ Backend/dotNetBackend/MainService/Program.cs
@@ -11,118 +11,71 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
 using dotNetBackend.Common;
 using MainService.EngineServices;
+using MainService;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        Title = "JWTToken_Auth_API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        
+        builder.Services.AddConfiguredSwagger();
+
+        builder.Services.AddDbContext<MasaProjectDbContext>(options =>
         {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                }
-            },
-            new string[] {}
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+        });
+
+        builder.Services.AddHttpClient();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                string TOKEN_SECRET = builder.Configuration.GetValue<string>("JwtSigningSecret")!;
+                var signingKey = Encoding.UTF8.GetBytes(TOKEN_SECRET);
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(signingKey),
+                };
+            });
+
+        builder.Services.AddConfiguredAuthorization();
+        
+        builder.Services.AddCors();
+
+        builder.Services.AddConfiguredServices();
+        
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    });
-});
 
-builder.Services.AddDbContext<MasaProjectDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    
-});
-builder.Services.AddHttpClient();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        string TOKEN_SECRET = builder.Configuration.GetValue<string>("JwtSigningSecret")!;
-        var signingKey = Encoding.UTF8.GetBytes(TOKEN_SECRET);
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(signingKey),
-        };
-    });
-builder.Services.AddAuthorization(options => 
-{
-    options.AddPolicy(Policies.BusinessOwner, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.BusinessOwner.ToString("D"));
-    });
-    options.AddPolicy(Policies.SocialActivist, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.SocialActivist.ToString("D"));
-    });
-    options.AddPolicy(Policies.NonprofitOrganization, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.NonprofitOrganization.ToString("D"));
-    });
-    options.AddPolicy(Policies.ProlobbyOwner, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.ProlobbyOwner.ToString("D"));
-    });
-    options.AddPolicy(Policies.SystemBackendOnly, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.System.ToString("D"));
-    });
-    options.AddPolicy(Policies.ProlobbyAndSystem, policy => {
-        policy.RequireClaim(TokenClaims.UserTypeId, UserTypes.ProlobbyOwner.ToString("D"), UserTypes.System.ToString("D"));
-    });
+        app.UseHttpsRedirection();
+        app.UseCors(
+            options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
-});
-builder.Services.AddCors();
+        // added logging of http requests
+        app.UseHttpLogging();
 
-// TODO: add auto IOC here
-builder.Services.AddScoped<SocialActivistService>();
-builder.Services.AddScoped<UserToCampaignBalanceService>();
-builder.Services.AddScoped<ProductToCampaignQtyService>();
-builder.Services.AddScoped<TransactionService>();
-builder.Services.AddScoped<TransactionValidationService>();
-builder.Services.AddScoped<BusinessOwnerService>();
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddScoped<DonationValidationService>();
-builder.Services.AddSingleton<TwitterEngineService>();
-builder.Services.AddSingleton<TimerService>();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-var app = builder.Build();
+        app.MapControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseCors(
-    options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-
-// added logging of http requests
-app.UseHttpLogging();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
